4bbc76249e81a4a57b00bb4197b341bb273e595e
vanethos <solid.goncalo@gmail.com>
2019-03-27 20:10:11 +0100

    Refactored Code - Added "UI" folder - Added "app.dart" file for App initialization - Changed "MyApp" to "WarrantyApp" - Changed "MyHomePage" to "LoginPage" - Removed tests folder

---
+++ lib/ui/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:warranty_app_example/ui/login/login_page.dart';
+
+class WarrantyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
