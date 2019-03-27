768f987a190c0da058d3b474386f640cbb245c9c
vanethos <solid.goncalo@gmail.com>
2019-03-27 23:17:14 +0100

    Added Button on the bottom of the page with SafeArea

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -12,6 +12,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // to be the same color as the image background
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -19,7 +21,29 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset("assets/warranty-logo.png")
+            // gets the image asset from the assets folder
+            Image.asset("assets/warranty-logo.png"),
+            // Uses all the remaining space in the Scaffold
+            Expanded(
+              // aligns the button to the bottom of the expanded space
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                // A widget that "avoids operating system interfaces" eg:
+                // iPhone X home button area
+                child: SafeArea(
+                  // using a container to add bottom margin
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 8),
+                    child: RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        // the action that occur on button press
+                        onPressed: () => print(""),
+                        child: Text("Login"),
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
