9a860e34064a34d3c5cf262f2db15cbce0e8c417
vanethos <solid.goncalo@gmail.com>
2019-03-28 22:36:12 +0100

    Added mock data Added Appbar Added ListView that maps the mock data into a Text widget

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:warranty_app_example/data/models/warranty.dart';
+
+var mockData = <Warranty>[
+  Warranty("Warranty 1", "Company 1", 1553808560),
+  Warranty("Warranty 2", "Company 1", 1502539200),
+  Warranty("Warranty 3", "Company 3", 1502529200)
+];
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,6 +17,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red,);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Warranties"),
+      ),
+      body: ListView(
+        children: mockData.map((warranty) => Text(warranty.name)).toList(),
+      ),
+    );
   }
 }
