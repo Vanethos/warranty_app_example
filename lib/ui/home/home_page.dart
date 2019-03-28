c97d1270d212699be3568a05935e78d8a70f097d
vanethos <solid.goncalo@gmail.com>
2019-03-28 22:56:53 +0100

    Test with 100 items

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:warranty_app_example/data/models/warranty.dart';
 
-var mockData = <Warranty>[
-  Warranty("Warranty 1", "Company 1", 1553808560),
-  Warranty("Warranty 2", "Company 1", 1502539200),
-  Warranty("Warranty 3", "Company 3", 1502529200)
-];
+var mockData = generateWarranties();
 
 class HomePage extends StatefulWidget {
   @override
@@ -49,7 +45,7 @@ class _WarrantyItem extends StatelessWidget {
                 children: <Widget>[
                   Text("Name: ${warranty.name}", style: TextStyle(fontWeight: FontWeight.bold)),
                   Text("Company: ${warranty.company}"),
-                  Text("Name: ${DateTime.fromMillisecondsSinceEpoch(warranty.date*1000)}"),
+                  Text("Name: ${DateTime.fromMillisecondsSinceEpoch(warranty.date)}"),
                 ],
               )
             ],
@@ -58,4 +54,12 @@ class _WarrantyItem extends StatelessWidget {
       ),
     );
   }
+}
+
+List<Warranty> generateWarranties() {
+  var warranties = <Warranty>[];
+  for (var i = 1; i < 100; i++) {
+    warranties.add(Warranty("Warranty $i", "Company $i", (DateTime.now().millisecondsSinceEpoch - i * 1000)));
+  }
+  return warranties;
 }
