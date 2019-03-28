daf33fc3485d802eaf8b9384d9878ff018f689a2
vanethos <solid.goncalo@gmail.com>
2019-03-28 20:30:05 +0100

    Created a new empty page with red background

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red,);
+  }
+}
