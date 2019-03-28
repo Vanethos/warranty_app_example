0c74be9619721cbc183ee288fc677bfb3dacaaa2
vanethos <solid.goncalo@gmail.com>
2019-03-28 20:42:00 +0100

    Changed navigation to use named routes

--- lib/ui/navigation.dart
+++ lib/ui/navigation.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:warranty_app_example/ui/home/home_page.dart';
 
+const routeLogin = "/";
+const routeHome = "home";
+
 Future<void> navigateToHomePage(BuildContext context) {
   return Navigator.of(context)
-      .pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+      .pushReplacementNamed(routeHome);
 }
